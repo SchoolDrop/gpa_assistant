7868b87c9c8756907e93973fb22fd06323e54eea
ForRate <info@foreignrateapi.com>
2026-04-17 14:15:39 -0700

    fixing final touch 3

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const bot = new TelegramBot(process.env.TEL_KEY);
+const bot = new TelegramBot(process.env.TEL_KEY, {
+  polling: true,
+});
 
 export default bot;
