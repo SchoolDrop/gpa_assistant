2a1020c0484717c31d4ffef3b3c079cae4865c07
ForRate <info@foreignrateapi.com>
2026-04-18 08:00:40 -0700

    making adjustment2

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@ import { dataSaved, histories, historiesCalc } from "./schema.js";
 import bot from "./bot.js";
 import express from "express";
 import dotenv from "dotenv";
+import { initBot } from "./botCode.js";
 
 dotenv.config();
 
@@ -10,8 +11,8 @@ const app = express();
 
 let initialized = false;
 
-app.post("/", (req, res) => {
-  initBot();
+initBot();
+app.post("/bot", (req, res) => {
   bot.processUpdate(req.body);
   res.status(200);
 });
